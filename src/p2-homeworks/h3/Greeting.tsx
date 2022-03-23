258858a57aae1502de5f7ae58794a38622915cbf
Antonov Kirill <antonovkir2@gmail.com>
2022-03-23 17:11:20 +0500

    styling HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -3,7 +3,7 @@ import s from './Greeting.module.css'
 
 type GreetingPropsType = {
     name: string
-    setNameCallback: (e:ChangeEvent<HTMLInputElement>) => void
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
     addUser: () => void
     error: string
     totalUsers: number
@@ -15,11 +15,15 @@ const Greeting: React.FC<GreetingPropsType> = (
     const inputClass = error ? s.error : ''
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.inputBlock}>
+            <div className={s.inputWrapper}>
+                <input value={name} onChange={setNameCallback} className={inputClass}/>
+                <span>{error}</span>
+            </div>
+
+                <button className={s.btn} onClick={addUser}>add</button>
+                <span>{totalUsers}</span>
+
         </div>
     )
 }
