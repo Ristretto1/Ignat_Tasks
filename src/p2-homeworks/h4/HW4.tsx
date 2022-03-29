3ed909f315509529e848cf91c9f68f736470508a
Antonov Kirill <antonovkir2@gmail.com>
2022-03-29 12:27:02 +0500

    styling superInput

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -31,10 +31,12 @@ function HW4() {
                     onEnter={showAlert}
                     error={error}
                     // spanClassName={s.testSpanError}
+                    placeholder={'Введите значение'}
                 />
 
                 <SuperInputText
                     className={s.blue} // проверьте, рабоет ли смешивание классов
+                    placeholder={'Введите значение'}
                 />
 
                 {/*----------------------------------------------------*/}
