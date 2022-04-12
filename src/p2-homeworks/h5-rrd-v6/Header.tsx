20e776fadc20db94885f86183a8d3c23b44afa16
Antonov Kirill <antonovkir2@gmail.com>
2022-04-12 19:00:38 +0500

    refactor

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,25 +4,27 @@ import {PATH} from './Pages';
 import s from './Header.module.css';
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
 
+type ObjectActiveClassName = {
+    isActive: boolean
+}
 
 function Header() {
 
     const [menuFlag, setMenuFlag] = useState<boolean>(true)
-
     const onClickMenuHandler = () => setMenuFlag(!menuFlag)
-
+    const isActive = ({isActive}: ObjectActiveClassName) => isActive ? s.active : ''
 
     return (
         <div>
             <ul className={s.menuList}>
-                <li className={menuFlag ? s.hidden : ''}>
-                    <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+                <li className={menuFlag ? `${s.hidden} ${s.menuListLink}` : s.menuListLink}>
+                    <NavLink className={isActive} to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
                 </li>
                 <li className={menuFlag ? s.hidden : ''}>
-                    <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+                    <NavLink className={isActive} to={PATH.JUNIOR}>Junior</NavLink>
                 </li>
                 <li className={menuFlag ? s.hidden : ''}>
-                    <NavLink to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
+                    <NavLink className={isActive} to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
                 </li>
                 <li>
                     <SuperButton onClick={onClickMenuHandler}>Menu</SuperButton>
