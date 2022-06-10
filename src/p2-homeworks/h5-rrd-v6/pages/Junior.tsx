9f7f8e68189f529255c13fd51de16a3b0db92b8d
Antonov Kirill <antonovkir2@gmail.com>
2022-06-10 16:41:00 +0500

    move hw7 hw8 in junior page

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,10 +1,13 @@
 
 import React from 'react';
+import HW7 from '../../h7/HW7';
+import HW8 from '../../h8/HW8';
 
 const Junior = () => {
     return (
         <div>
-            <h1>Домашние задания Джун</h1>
+            <HW7/>
+            <HW8/>
         </div>
     );
 };
