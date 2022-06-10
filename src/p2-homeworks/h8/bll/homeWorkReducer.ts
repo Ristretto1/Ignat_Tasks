6f4f1c5039e20c1cd19b331cdfcb1cea55c85811
Antonov Kirill <antonovkir2@gmail.com>
2022-06-10 16:41:14 +0500

    h8 task solution

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,29 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type ActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+} | {
+    type: 'check'
+    payload: number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            switch(action.payload) {
+                case 'down':
+                    return [...state].sort((a, b) => b.age - a.age)
+                case 'up':
+                    return [...state].sort((a, b) => a.age - b.age)
+                default: return state
+            }
         }
+
         case 'check': {
-            // need to fix
-            return state
+            return [...state].filter(p => action.payload < p.age)
         }
-        default: return state
+        default:
+            return state
     }
 }
