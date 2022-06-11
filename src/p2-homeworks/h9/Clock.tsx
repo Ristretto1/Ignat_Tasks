0e53199e287d4cb2c91be3a2c3f35d83a73c32e6
Antonov Kirill <antonovkir2@gmail.com>
2022-06-11 17:41:26 +0500

    create Clock

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,41 +3,51 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+       clearInterval(timerId)
     }
+
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    let finishDate = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+    let finishMonth = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth()
+
+    let finishHours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+    let finishMinutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+    let finishSeconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+
+    const stringTime = `${finishHours}:${finishMinutes}:${finishSeconds} `
+    const stringDate = `${finishDate}.${finishMonth}.${date.getFullYear()} `
 
     return (
         <div>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
+                style={{margin: '0 15px', border: '1px solid #ccc', width: '70px', textAlign:'center', padding: '2px', verticalAlign: 'middle'}}
             >
                 {stringTime}
             </div>
 
             {show && (
-                <div>
+                <div style={{margin: '0 15px', border: '1px solid #ccc', width: '80px', textAlign:'center', padding: '2px', verticalAlign: 'middle'}}
+                >
                     {stringDate}
                 </div>
             )}
