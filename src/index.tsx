874d4edaa7c58f736174736fe8543f5ad7a298f8
Kirill Antonov <antonovkir2@gmail.com>
2023-01-22 16:43:16 +0500

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './p1-main/m1-ui/u1-app/App'
-import * as serviceWorker from './serviceWorker'
-import {Provider} from 'react-redux';
-import store from './p2-homeworks/h10/bll/store';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-// igg2
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
--- src/p2-homeworks/h10/Preloader.tsx
+++
@@ -1,6 +0,0 @@
-import preloader from "./preloader.svg";
-import React from "react";
-
-export const Preloader = () => {
-    return <img src={preloader}/>
-}
