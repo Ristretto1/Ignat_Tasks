76c573c1953c3b2ebca005b357b8f0dd7724b82b
Kirill Antonov <antonovkir2@gmail.com>
2023-03-09 15:30:45 +0500

    add 2d task

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -51,8 +51,9 @@ export const friendMessage0: MessageType = {
 
 const HW1 = () => {
 	return (
-		<div id={'hw1'} style={{ maxWidth: 1024, margin: '0 auto' }}>
+		<div id={'hw1'}>
 			<div className={s2.hwTitle}>Homework #1</div>
+			<hr />
 			<div className={s2.hw}>
 				{/*проверка отображения (не менять)*/}
 				<div>
@@ -63,6 +64,7 @@ const HW1 = () => {
 				{/*для автоматической проверки дз (не менять)*/}
 				<MessageSender M={Message} />
 			</div>
+			<hr />
 		</div>
 	)
 }
