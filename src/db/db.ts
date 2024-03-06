8908acf71022625d7313947ee2da154c8815609b
Antonov Kirill <antonovkir2@gmail.com>
2024-03-06 19:32:54 +0500

    fix:

--- src/db/db.ts
+++ src/db/db.ts
@@ -9,7 +9,7 @@ const database = client.db();
 export const blogCollection = database.collection<IBlogDB>('blogs');
 export const postCollection = database.collection<IPostDB>('posts');
 export const userCollection = database.collection<IUserDB>('users');
-export const commentCollection = database.collection<ICommentDB>('comment');
+export const commentCollection = database.collection<ICommentDB>('comments');
 
 export const runDb = async () => {
   try {
